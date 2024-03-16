38e3c88fb0c5bb971d0c37e7d52dcb85b9fe894c
Gabriela Zorzo <gabriela_zorzo@terceiros.sicredi.com>
2024-03-16 18:01:19 -0300

    start

---
+++ MainApp/MainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainAppApp.swift
+//  MainApp
+//
+//  Created by Gabriela Zorzo on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
